9ade0ce3dcfd1cc637dda1c15155804d05bb3293
Dennis Byberg <dennis.byberg@hotmail.com>
2024-10-21 00:01:38 +0200

    Add user creation endpoint and database interaction functions

--- backend/services.py
+++ backend/services.py
@@ -1,4 +1,9 @@
+import sqlalchemy.orm as _orm
+import passlib.hash as _hash
+
 import database as _database
+import models as _models
+import schemas as _schemas
 
 
 # Function to create the database tables
@@ -7,3 +12,27 @@ def create_database():
     # bind=_database.engine:
     # Use the engine defined in the database module to connect to the database
     return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+def get_db():
+    db = _database.SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+
+
+async def get_user_by_email(email: str, db: _orm.Session):
+    return db.query(_models.User).filter(_models.User.email == email).first()
+
+
+async def create_user(user: _schemas.UserCreate, db: _orm.Session):
+    user_obj = _models.User(
+        email=user.email, hashed_password=_hash.bcrypt.hash(user.hashed_password)
+    )
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
